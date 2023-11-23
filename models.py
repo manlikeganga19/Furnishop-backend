b1d251289fbd100510e1c7020274d5511faf7738
Daniel <owenjodaniel19@gmail.com>
2023-11-23 12:53:42 +0300

    created models

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    cart = db.relationship('CartItem', backref='user', lazy=True)
+
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item_name =db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.Foreignkey('user.id'), nullable=False)
+                                   
